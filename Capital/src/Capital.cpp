5f6b509fbf991b9a7bd2a1fbab907c226502398e
Egor <egorkomar@yandex.ru>
2019-01-29 17:05:41 +0300

    2901 change

--- Capital/src/Capital.cpp
+++ Capital/src/Capital.cpp
@@ -8,15 +8,27 @@
 
 #include <iostream>
 #include <vector>
-#include <map>;
+#include <map>
 
 using namespace std;
 
-void ChangeCapitalFunc() {
+void ChangeCapitalFunc(const string& country, const string& new_capital, map<string,string>& c_info) {
+
+	//map<string,string> :: iterator it;
+	//it = c_info.find(country);
+	if (c_info.find(country) == c_info.end()) {
+		cout << "Introduce new country " << country << " with capital " << new_capital << endl;
+		c_info[country]=new_capital;
+	}
+	else if (c_info.find(country)->second == new_capital)
+		cout << "Country " << country << " hasn't changed its capital" << endl;
+	else if (c_info.find(country)->second != new_capital)
+		cout << "Country " << country << " has changed its capital from " << c_info[country] << " to " << new_capital << endl;
+
 
 }
 
-void RenameFunc () {
+void RenameFunc (const string& old_country_name, const string& new_country_name, map<string,string>& c_info) {
 
 }
 
@@ -31,17 +43,19 @@ void DumpFunc() {
 int main() {
 	int num;
 	cin >> num;
+	map<string,string> c_info;
 	string action;
+	for (int i = 0; i < num; i++) {
 	cin >> action;
 	if (action == "CHANGE_CAPITAL") {
 		string country, new_capital;
 		cin >> country >> new_capital;
-		ChangeCapitalFunc();
+		ChangeCapitalFunc(country,new_capital,c_info);
 	}
 	else if (action == "RENAME") {
 		string old_country_name, new_country_name;
 		cin >> old_country_name >> new_country_name;
-		RenameFunc();
+		RenameFunc(old_country_name,new_country_name,c_info);
 	}
 	else if (action == "ABOUT") {
 		string country;
@@ -51,6 +65,6 @@ int main() {
 	else if (action == "DUMP")
 		DumpFunc();
 
-
+	}
 	return 0;
 }

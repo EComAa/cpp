f9adee329201ced091b954bf9c87beb5006388da
Egor <egorkomar@yandex.ru>
2019-01-30 16:56:52 +0300

    30102019

--- Bus_station/src/Bus_station.cpp
+++ Bus_station/src/Bus_station.cpp
@@ -12,8 +12,51 @@
 
 using namespace std;
 
+void NewBusFunc () {
+
+}
+
+void BusesForStopFunc () {
+
+}
+
+void StopsForBus () {
+
+}
+
+void  AllBusesfunc () {
+
+}
+
 int main() {
 	int num;
 	cin >> num;
+	map<string,vector<string>> bus_map;
+	string action;
+	cin >> action;
+	if (action == "NEW_BUS") {
+		string bus_name;
+		cin >> bus_name;
+		int stop_count;
+		cin >> stop_count;
+		vector<string> bus_station;
+		for (int i = 0; i < stop_count; i++ ) {
+			string st;
+			cin >> st;
+			bus_station.push_back(st);
+		}
+	//	for (auto i: bus_station)
+	//		cout << i << " ";
+		bus_map[bus_name]=bus_station;
+		NewBusFunc();
+	}
+	else if (action == "BUSES_FOR_STOP")
+		BusesForStopFunc();
+	else if(action == "STOPS_FOR_BUS")
+		StopsForBus();
+	else if (action == "ALL_BUSES")
+		AllBusesfunc();
+
 	return 0;
 }
+;

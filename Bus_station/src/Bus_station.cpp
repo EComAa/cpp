bbf40eaddf5aa8444534f008c7d6bc130627203a
Egor <egorkomar@yandex.ru>
2019-01-30 13:07:12 +0300

    add bus_station project

---
+++ Bus_station/src/Bus_station.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Bus_station.cpp
+// Author      : Egro Koamrov
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	return 0;
+}

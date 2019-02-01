502337938008c47fcba7c3560ba7929e6619bbc9
Egor <egorkomar@yandex.ru>
2019-02-01 14:59:43 +0300

    01022019

--- Bus_station/src/Bus_station.cpp
+++ Bus_station/src/Bus_station.cpp
@@ -12,19 +12,81 @@
 
 using namespace std;
 
-void NewBusFunc () {
+
+
+void BusesForStopFunc (const map<string,vector<string>>& bus_map,const string& station) {
+	vector<string> enum_bus;
+
+	for (auto i : bus_map) {
+		for (auto s : i.second) {
+			if (s == station)
+				enum_bus.push_back(i.first);
+		}
+	}
+	if( enum_bus.size() != 0 ) {
+		for (auto i : enum_bus)
+			cout << i << " ";
+	}
+	else
+		cout << "No stop";
+	cout << endl;
+}
+
+void StopsForBus (map<string,vector<string>>& bus_map, const string& bus) {
+	map<string,vector<string>> interchange;  // pair   bus : station, station, station ...
+
+	if (bus_map.find(bus) == bus_map.end())
+		cout << "No bus" << endl;
+	else {
+	vector<string> station = bus_map[bus];    //поместил все остановки в вектор
+	vector<string> un_station;
+	for (auto st : station) { // начал перебирать элекменты вектора остановок
+
+		for ( auto i : bus_map) { // для каждого автобуса
+
+		  for (auto s : i.second) { // смотрю каждую из его остановок
+			if (st == s && i.first != bus) { // если остановка из списка икомого автобуса совпадает с остановкой другого автобуса то добавляем
+				un_station.push_back(i.first);
+				interchange[s]=un_station;
+			}
+		}
+		 if (interchange.size() == 0 )
+			cout << i.first << " : no interchange" << endl;
+		 else {
+		 for (auto i : interchange) {
+			cout << endl << "Stop " << i.first <<": ";
+			for (auto stop : i.second)
+				cout << " " << stop;
+		 }
+	  }
+	  un_station.clear();
+
+	}
+	}
+	if (interchange.size() == 0 && bus_map.find(bus) != bus_map.end())
+		cout << "No interchange";
+	else {
+		for (auto i : interchange) {
+			cout << endl << "Stop " << i.first <<": ";
+			for (auto stop : i.second)
+				cout << " " << stop;
+		}
+	}
+	}
 
 }
 
-void BusesForStopFunc () {
+void  AllBusesfunc (const map<string,vector<string>>& bus_map) {
+	if (bus_map.size() == 0)
+		cout << "No buses" << endl;
+	else {
+		for (auto i : bus_map) {
+			cout << endl << "Bus " << i.first << ": ";
+			for (auto s : i.second)
+				cout << s << " ";
+		}
+	}
 
-}
-
-void StopsForBus () {
-
-}
-
-void  AllBusesfunc () {
 
 }
 
@@ -32,8 +94,11 @@ int main() {
 	int num;
 	cin >> num;
 	map<string,vector<string>> bus_map;
-	string action;
-	cin >> action;
+
+	for ( int i = 0; i < num; i++) { // открывающие скобки общего ввода
+		string action;
+		cin >> action;
+
 	if (action == "NEW_BUS") {
 		string bus_name;
 		cin >> bus_name;
@@ -48,15 +113,22 @@ int main() {
 	//	for (auto i: bus_station)
 	//		cout << i << " ";
 		bus_map[bus_name]=bus_station;
-		NewBusFunc();
 	}
-	else if (action == "BUSES_FOR_STOP")
-		BusesForStopFunc();
-	else if(action == "STOPS_FOR_BUS")
-		StopsForBus();
+	else if (action == "BUSES_FOR_STOP") {
+		string station;
+		cin >> station;
+		BusesForStopFunc(bus_map, station);
+	}
+	else if(action == "STOPS_FOR_BUS") {
+		string bus;
+		cin >> bus;
+		StopsForBus(bus_map, bus);
+	}
 	else if (action == "ALL_BUSES")
-		AllBusesfunc();
+		AllBusesfunc(bus_map);
 
+	} //закрывающие скобки общего ввода
 	return 0;
+
 }
-;
+
